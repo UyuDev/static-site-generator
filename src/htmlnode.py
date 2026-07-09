da5f501ba4218b1d9d9fe68e81e8db5286d418b9
Milkftw <282414362+Milkftw@users.noreply.github.com>
2026-07-09 16:10:59 -0700

    Implement Leaf nodes and respective unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,29 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str, value: str, props: dict=None):
+        super().__init__(tag, value, props=props)
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError
+        if self.tag is None:
+            return self.value
+        # for links and images the spacing is different so I separated them
+        if self.tag == 'a':
+            return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+        elif self.tag == 'img':
+            return f'<{self.tag}{self.props_to_html()} />'
+        elif self.props is None:
+            return f'<{self.tag}>{self.value}</{self.tag}>'
+        else:
+            return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+       
+
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr_none(self):
@@ -35,3 +35,26 @@ class TestHTMLNode(unittest.TestCase):
         string1 = node.props_to_html()
         string2 = ' href="https://www.google.com" target="_blank"'
         self.assertEqual(string1, string2)
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_img(self):
+        node = LeafNode("img", "", {"src": "url/of/image.jpg", "alt": "Description of image"})
+        self.assertEqual(node.to_html(), '<img src="url/of/image.jpg" alt="Description of image" />')
+
+    def test_leaf_repr_none(self):
+        node = LeafNode(None, None, None)
+        string1 = f"LeafNode({node.tag}, {node.value}, {node.props})"
+        self.assertEqual(string1, "LeafNode(None, None, None)")
+    
+    def test_leaf_repr_text(self):
+        node = LeafNode(None, "Hello", None)
+        string1 = f"LeafNode({node.tag}, {node.value}, {node.props})"
+        self.assertEqual(string1, "LeafNode(None, Hello, None)")
