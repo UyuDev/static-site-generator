324cfc3e0bc95ee293fa42c6618a7d2e033d928c
Milkftw <282414362+Milkftw@users.noreply.github.com>
2026-07-15 17:16:28 -0700

    Add configurable basepath via sys.argv

--- src/gen_content.py
+++ src/gen_content.py
@@ -4,7 +4,7 @@ import os
 
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str):
+def generate_page(from_path: str, template_path: str, dest_path: str, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as file:
         markdown_contents = file.read()
@@ -15,9 +15,11 @@ def generate_page(from_path: str, template_path: str, dest_path: str):
     html_text = parent_node.to_html()
     replace_title = template_contents.replace("{{ Title }}", title)
     replace_content = replace_title.replace("{{ Content }}", html_text)
+    replace_href = replace_content.replace('href="/', f'href="{basepath}')
+    replace_src = replace_href.replace('src="/', f'src="{basepath}')
     check_directories(dest_path)
     with open(dest_path, "w") as file:
-        file.write(replace_content)
+        file.write(replace_src)
 
 def check_directories(target_path: str):
     parent_dir = os.path.dirname(target_path)
@@ -25,7 +27,7 @@ def check_directories(target_path: str):
         os.makedirs(parent_dir, exist_ok=True)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     print(f"Checking items in directory: {dir_path_content}")
     for item_name in os.listdir(dir_path_content):
         print(f"Checking item: {item_name}")
@@ -40,7 +42,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                 new_item_name = new_item_name.replace(".md", ".html")
             print(f"Generating a new file for {item_name} in directory:{dest_dir_path}")
             new_dest_path = os.path.join(dest_dir_path, new_item_name)
-            generate_page(full_path, template_path, new_dest_path)
+            generate_page(full_path, template_path, new_dest_path, basepath)
         print(f"Checking if {full_path} is a directory: {os.path.isdir(full_path)}")
         if os.path.isdir(full_path):
             new_dest_path = os.path.join(dest_dir_path, item_name)
@@ -48,4 +50,4 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             os.mkdir(new_dest_path)
             print(f"Making new directory: {new_dest_path}")
             print(f"Recursively calling generate_pages_recursive with source: {full_path} and target: {new_dest_path}")
-            generate_pages_recursive(full_path, template_path, new_dest_path)
+            generate_pages_recursive(full_path, template_path, new_dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 from copy_static import copy_static
 from gen_content import generate_pages_recursive
+import sys
 
 def main():
+    if len(sys.argv) == 1:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1]
     source_dir = "static"
     target_path = "public"
     template = "template.html"
     origin_dir = "content"
     target_dir = "public"
     copy_static(source_dir, target_path)
-    generate_pages_recursive(origin_dir, template, target_dir)
+    generate_pages_recursive(origin_dir, template, target_dir, basepath)
 
 
 if __name__ == "__main__":
